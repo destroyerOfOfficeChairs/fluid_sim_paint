a37fbc2d53e480f9315e1a77d99b1f9f098c73b5
destroyerOfOfficeChairs <wjbvii@gmail.com>
2026-01-18 15:19:25 -0600

    Move ping-pong texture setup to new file.

--- src/canvas/state.rs
+++ src/canvas/state.rs
@@ -1,5 +1,6 @@
 use super::state_helpers::compute_pipeline::create_compute_setup;
 use super::state_helpers::render_pipeline::create_render_setup;
+use super::state_helpers::resource_setup::create_ping_pong_textures;
 use super::state_helpers::wgpu_init::wgpu_init;
 use crate::canvas::quad::*;
 use crate::texture;
@@ -47,62 +48,8 @@ impl State {
     pub async fn new(window: Arc<Window>) -> anyhow::Result<State> {
         let (surface, device, queue, config) = wgpu_init(window.clone()).await;
 
-        // -----------------------------------------------------------------------
-        // 1. Create the Ping-Pong Textures
-        // -----------------------------------------------------------------------
-        // We use a fixed grid size for the simulation (e.g., 512x512)
-        // This is independent of the window size!
-        let sim_width = 512;
-        let sim_height = 512;
+        let (texture_a, texture_b) = create_ping_pong_textures(&device, &queue);
 
-        let texture_a = texture::Texture::create_storage_texture(
-            &device,
-            sim_width,
-            sim_height,
-            Some("Texture A"),
-        );
-        let texture_b = texture::Texture::create_storage_texture(
-            &device,
-            sim_width,
-            sim_height,
-            Some("Texture B"),
-        );
-
-        // -----------------------------------------------------------------------
-        // 2. Initial Data Upload (Random Noise)
-        // -----------------------------------------------------------------------
-        // We fill Texture A with random noise so we have something to fade.
-        let mut initial_data = Vec::with_capacity((sim_width * sim_height * 4) as usize);
-        for _ in 0..(sim_width * sim_height) {
-            let r: u8 = rand::random();
-            let g: u8 = rand::random();
-            let b: u8 = rand::random();
-            initial_data.extend_from_slice(&[r, g, b, 255]);
-        }
-
-        queue.write_texture(
-            wgpu::TexelCopyTextureInfo {
-                texture: &texture_a.texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            &initial_data,
-            wgpu::TexelCopyBufferLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * sim_width),
-                rows_per_image: Some(sim_height),
-            },
-            wgpu::Extent3d {
-                width: sim_width,
-                height: sim_height,
-                depth_or_array_layers: 1,
-            },
-        );
-
-        // -----------------------------------------------------------------------
-        // 3. Pipeline Setup (Refactored)
-        // -----------------------------------------------------------------------
         let (compute_pipeline, compute_bind_group_a, compute_bind_group_b) =
             create_compute_setup(&device, &texture_a, &texture_b);
 
--- src/canvas/state_helpers/mod.rs
+++ src/canvas/state_helpers/mod.rs
@@ -1,3 +1,4 @@
 pub mod compute_pipeline;
 pub mod render_pipeline;
+pub mod resource_setup;
 pub mod wgpu_init;
